2e8eae35f6ec73d4bef75d58e5684c2b9ed92f37
[NCMS] 이재진 <1700259@partner.skcc.com>
2019-03-07 16:13:32 +0900

    20190307

---
+++ jins/src/test/java/TestCase.java
@@ -0,0 +1,118 @@
+import org.junit.Assert;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+
+@SpringBootTest
+public class TestCase {
+
+    public static void main(String [] args){
+
+//        Money five = new Dollar(2).times(5);
+//        Assert.assertEquals(new Dollar(10), five.times(2));
+
+    }
+
+    @Test
+    public void test(){
+//        Assert.assertTrue(new Dollar(5).equals(new Dollar(5)));
+//        Assert.assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
+    @Test
+    public void testEquality(){
+        Assert.assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    }
+
+    @Test
+    public void testMultiplication(){
+//        Money five = new Dollar(5);
+        //Dollar product = five.times(2);
+        //Assert.assertEquals(10, product.getAmount());
+        //Assert.assertEquals(new Dollar(10), product);
+        //product = five.times(3);
+        //Assert.assertEquals(15, product.getAmount());
+        //Assert.assertEquals(new Dollar(15), product);
+//        Assert.assertEquals(new Dollar(10), five.times(2));
+//        Assert.assertEquals(new Dollar(15), five.times(3));
+//        Assert.assertTrue(Money.dollar(5).equals(new Dollar(5)));
+//        Assert.assertFalse(Money.dollar(5).equals(new Dollar(6)));
+//        Assert.assertTrue(Money.franc(5).equals(new Franc(5)));
+//        Assert.assertFalse(Money.franc(5).equals(new Franc(6)));
+//        Assert.assertFalse(Money.franc(5).equals(Money.dollar(5)));
+
+        Assert.assertTrue(new Money(10, "CHF").equals(new Money(10, "CHF")));
+        Assert.assertTrue(new Money(10, "CHF").times(2).equals(new Money(10, "CHF").times(2)));
+        Assert.assertNotEquals(Money.dollar(10).currency(), Money.franc(10).currency());
+
+    }
+
+    @Test
+    public void testSimpleAddition(){
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        Assert.assertEquals(Money.dollar(10),reduced);
+
+    }
+
+
+
+}
+
+class Money implements Expression{
+
+    Expression plus(Money addend){
+        return new Money(amount + addend.amount, currency);
+    }
+
+    Money(int amount, String currency){
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected int amount;
+
+    protected String currency;
+
+    public boolean equals(Object object){
+        Money money = (Money)object;
+        return amount == money.amount
+                && currency().equals(money.currency());
+    }
+
+    static Money dollar(int amount){
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount){
+        return new Money(amount, "CHF");
+    }
+
+    String currency(){
+        return currency;
+    }
+
+    Money times(int multiplier){
+        return new Money(amount * multiplier, currency);
+    };
+
+    public String toString(){
+        return amount + " " + currency;
+    }
+
+//    Money plus(Money addend){
+//        return new Money(amount + addend.amount, currency);
+//    }
+
+}
+
+interface Expression{
+
+}
+
+class Bank{
+    Money reduce(Expression source, String to){
+        return Money.dollar(10);
+    }
+}
